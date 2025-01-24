a11fe75aa7ab5745dd4864db7c54161a496fef81
alastafew <alexastafew@gmail.com>
2025-01-24 17:04:03 +0300

    UI and code

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -7,13 +7,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
+    @IBOutlet var redLightView: UIView!
+    @IBOutlet var yellowLightView: UIView!
+    @IBOutlet var greenLightView: UIView!
+    @IBOutlet var lightButton: UIButton!
+    
+    private var pushLightButtonCount = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redLightView.layer.cornerRadius = 50
+        yellowLightView.layer.cornerRadius = 50
+        greenLightView.layer.cornerRadius = 50
+        
+        redLightView.alpha = 0.2
+        yellowLightView.alpha = 0.2
+        greenLightView.alpha = 0.2
     }
-
-
+    
+    @IBAction func changeLightButton(_ sender: UIButton) {
+        
+        pushLightButtonCount += 1
+        
+        switch pushLightButtonCount{
+            
+        case 1: redLightView.alpha = 1.0
+            yellowLightView.alpha = 0.2
+            greenLightView.alpha = 0.2
+            lightButton.setTitle("Next", for: .normal)
+            
+        case 2: yellowLightView.alpha = 1.0
+            redLightView.alpha = 0.2
+            greenLightView.alpha = 0.2
+            
+        default: greenLightView.alpha = 1.0
+            redLightView.alpha = 0.2
+            yellowLightView.alpha = 0.2
+            pushLightButtonCount = 0
+        }
+    }
 }
 
